11132b446db7cf8d7af35f6dc08aa32913efecb8
yuichiro-cloud <ghjhbc1@gmail.com>
2020-01-15 20:05:34 +0900

    users_groups-table-created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200115105618) do
+ActiveRecord::Schema.define(version: 20200115110318) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       default: "", null: false
@@ -31,4 +31,15 @@ ActiveRecord::Schema.define(version: 20200115105618) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "users_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_users_groups_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_users_groups_on_user_id", using: :btree
+  end
+
+  add_foreign_key "users_groups", "groups"
+  add_foreign_key "users_groups", "users"
 end
